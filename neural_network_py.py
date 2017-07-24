01584052acf7aea38cbb7074737ae1c25b588f77
nivosco <nivosco@gmail.com>
2017-07-24 21:12:41 +0300

    Update neural_network_py.py

--- neural_network_py.py
+++ neural_network_py.py
@@ -71,6 +71,38 @@ def costFunctionGradient(theta):
     theta_grad = np.append(theta1_grad.reshape(-1,1), theta2_grad.reshape(-1,1))
     return theta_grad
 
+def gradientDescent(theta):
+    m,n = X.shape
+    a1 = np.hstack((np.ones((X.shape[0], 1)), X))
+    delta3 = np.zeros((m, num_labels))
+    for k in range(0, iter_grad):
+        theta1 = (theta[:(hidden_layer_size * (input_layer_size + 1))]).reshape(hidden_layer_size, input_layer_size + 1)
+        theta2 = (theta[(hidden_layer_size * (input_layer_size + 1)):]).reshape(num_labels, hidden_layer_size + 1)
+        z2 = np.matmul(a1 ,theta1.T)
+        a2 = np.hstack((np.ones((z2.shape[0], 1)), sigmoid(z2)))
+        z3 = np.matmul(a2 ,theta2.T)
+        a3 = sigmoid(z3)
+        for i in range(0,m):
+            for j in range(1, num_labels + 1):
+                if (y.item(i,0) == j):
+                    delta3[i,j-1] = a3[i,j-1] - 1
+                else:
+                    delta3[i,j-1] = a3[i,j-1]
+        z2 = np.hstack((np.ones((z2.shape[0], 1)), z2))
+        delta2 = np.multiply(np.matmul(delta3, theta2), (np.multiply(sigmoid(z2), (1 - sigmoid(z2)))))
+        delta2 = (np.delete(delta2.T, 0, 0)).T
+        theta1_grad = (np.matmul(a1.T, delta2) / m).T
+        theta2_grad = (np.matmul(a2.T, delta3) / m).T
+        theta1_t = (np.delete(theta1.T, 0, 0)).T
+        theta1_t = np.hstack((np.zeros((theta1_t.shape[0], 1)), theta1_t))
+        theta2_t = (np.delete(theta2.T, 0, 0)).T
+        theta2_t = np.hstack((np.zeros((theta2_t.shape[0], 1)), theta2_t))
+        theta1_grad = theta1_grad + (lambda_reg / m) * theta1_t
+        theta2_grad = theta2_grad + (lambda_reg / m) * theta2_t
+        theta_grad = (np.append(theta1_grad.reshape(-1,1), theta2_grad.reshape(-1,1))).reshape(-1,1)
+        theta = theta - (alpha * theta_grad);
+    return theta
+
 def computeNumericalGradient(theta):
     numgrad = np.zeros(theta.shape)
     perturb = np.zeros(theta.shape)
@@ -105,6 +137,8 @@ num_labels = 10          # 10 labels, from 1 to 10 (10 represents 0)
 epsilon = 0.12           # Range for parameter initialization
 numerical_eps = 1e-4     # Numerical gradient parameter
 lambda_reg = 0.5         # Regularization parameter
+iter_grad = 1000         # Number of iterations to run gradient descent
+alpha = 1                # Learning rate for gradient descent
 
 ## Load the data
 data = scipy.io.loadmat('data.mat')  # trainig size of 5000 images
@@ -123,8 +157,9 @@ initial_nn_params = (np.append(initial_Theta1.reshape(-1,1), initial_Theta2.resh
 #print("Gradient checking value: ", diff)  # should be a small number ~ 1e-9
 
 ## Train the network
-res = minimize(costFunction, initial_nn_params, method='BFGS', jac=costFunctionGradient, options={'disp': True, 'maxiter': 50})
-nn_params = res.x
+#res = minimize(costFunction, initial_nn_params, method='BFGS', jac=costFunctionGradient, options={'disp': True, 'maxiter': 50})
+#nn_params = res.x
+nn_params = gradientDescent(initial_nn_params)
 
 ## check accuaracy
 pred = predict(nn_params)
